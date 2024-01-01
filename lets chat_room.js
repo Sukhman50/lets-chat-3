020bbc7605902689337655c983020d384e1000e4
Sukhman50 <140614380+Sukhman50@users.noreply.github.com>
2024-01-01 17:48:58 +0530

    Add files via upload

--- lets chat_room.js
+++ lets chat_room.js
@@ -20,7 +20,7 @@ const firebaseConfig = {
        Room_names = childKey;
       //Start code
      console.log("Room Name - "+ Room_names);
-     row = "<div class='room_name id="+Room_names+" onclick='redirectToRoomName(this.id)'>#"+ Room_names +"<div><hr>";
+     row = "<div class='room_name' id="+Room_names+" onclick='redirectToRoomName(this.id)'>#"+ Room_names +"<div><hr>";
      document.getElementById("output").innerHTML += row;
       //End code
       });});}
@@ -47,5 +47,5 @@ function redirectToRoomName(name)
 {
       console.log(name);
       localStorage.setItem("room_name", name);
-       window.location("letschat_page.html");
+       window.location="letschat_page.html";
 }
